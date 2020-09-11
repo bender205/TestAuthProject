f9218f321de28b26b7ce5136a9bae9701428b821
bender205 <sbezsm@gmail.com>
2020-09-11 17:51:27 +0300

    edited startup.cs TestAuth.Resource.Api

--- TestAuth.Resource.Api/Startup.cs
+++ TestAuth.Resource.Api/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using TestAuth.Auth.Common;
 using TestAuth.Resource.Api.Models;
 
@@ -22,35 +23,57 @@ namespace TestAuth.Resource.Api
             Configuration = configuration;
         }
       public IConfiguration Configuration { get; }
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
 
-            var authOptions = Configuration.GetSection("Auth").Get<AuthOptions>();
+      public void ConfigureServices(IServiceCollection services)
+      {
+          services.AddControllers();
+
+          var authOptions = Configuration.GetSection("Auth").Get<AuthOptions>();
+
+          services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+              .AddJwtBearer(options =>
+              {
+                  options.RequireHttpsMetadata = false;
+                  options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                  {
+                      ValidateIssuer = true,
+                      ValidIssuer = authOptions.Issuer,
+
+                      ValidateAudience = true,
+                      ValidAudience = authOptions.Audience,
+
+                      ValidateLifetime = true,
+
+                      IssuerSigningKey = authOptions.GetSymmetricSecurityKey(), //HS256
+                      ValidateIssuerSigningKey = true,
+                  };
+
+              });
 
 
-            services.AddCors(options =>
-            {
-                options.AddDefaultPolicy(
-                    builder =>
-                    {
-                        builder.AllowAnyOrigin()
-                            .AllowAnyMethod()
-                            .AllowAnyHeader();
-                    });
-            });
-            services.AddSingleton(new BookStore());
-        }
+          services.AddCors(options =>
+          {
+              options.AddDefaultPolicy(
+                  builder =>
+                  {
+                      builder.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader();
+                  });
+          });
+          services.AddSingleton(new BookStore());
+      }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+      // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseRouting();
+     app.UseRouting();
+            app.UseAuthorization();
+       
             app.UseCors();
             app.UseEndpoints(endpoints =>
             {
